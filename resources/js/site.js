b5b7684152a6dc4b3073b6628d7cec2eb8920611
Declan Murphy <declan.murphy@flightclub.io>
2020-10-22 14:48:34 -0500

    nav works for mobile. now only have an issue with the about layout

--- resources/js/site.js
+++ resources/js/site.js
@@ -4,6 +4,22 @@ Vue.component('spinner', {
     template: '<svg class="spinner animate-spin w-6 h-6" stroke="currentColor" viewBox="0 0 20 20"><g transform="translate(1.9,1.9)" fill-rule="evenodd" fill="none"><g stroke-width="2" transform="translate(1,1)"><ellipse ry="8.6124411" rx="8.6124392" style="stroke-width:2.77512;stroke-miterlimit:4;stroke-dasharray:none" cy="7.1" cx="7.1" stroke-opacity="0.5" /><path style="stroke-width:2.77512;stroke-linecap:round;stroke-miterlimit:4;stroke-dasharray:none"d="m 15.712439,7.1000019 c 0,-4.7559816 -3.856458,-8.6124415 -8.6124391,-8.6124415"/></g></g></svg>'
   })
 
+new Vue({
+    el: '#nav',
+    data() {
+        return {
+            mobileMenuOpen: false
+        }
+    },
+    methods: {
+
+        toggleMobileMenu() {
+            this.mobileMenuOpen = !this.mobileMenuOpen;
+        }
+    
+    }
+});
+
 var app = new Vue({
     el: '#contactForm',
     data() {
