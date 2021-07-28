438b89d30a9131cab345318cff9a879d801eb784
declan@washington <declan.murphy@flightclub.io>
2021-07-28 16:59:25 +0000

    add script for content backup. maybe other things that need to backed up, update if necessary

---
+++ scripts/content_backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -r /var/www/www.bridomeara.ie/content/* content/
+cp -r /var/www/www.bridomeara.ie/public/assets/* public/assets/
+cp -r /var/www/www.bridomeara.ie/resources/* resources/
