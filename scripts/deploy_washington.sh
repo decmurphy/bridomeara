c3e96d4167850de7d5261a1d0c73547de1c650d5
Declan Murphy <declan.murphy@flightclub.io>
2021-08-24 12:20:39 -0500

    better deploy script

--- scripts/deploy_washington.sh
+++ scripts/deploy_washington.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 npm run production
-cp -r public/* /var/www/www.bridomeara.ie/public/
-cp -r scripts/* /var/www/www.bridomeara.ie/scripts/
-cp -r resources/views/* /var/www/www.bridomeara.ie/resources/views/
-cp -r resources/css/* /var/www/www.bridomeara.ie/resources/css/
+sudo cp -r * /var/www/www.bridomeara.ie/
+cd /var/www/www.bridomeara.ie/ && sudo php artisan cache:clear && cd -
